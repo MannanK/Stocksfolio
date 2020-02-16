632fd82c95ebfeef4f686ed209e2a6de2657d5fc
Mannan Kasliwal <mkasliw1@binghamton.edu>
2020-02-16 18:19:26 -0500

    Add the beginnings of backend auth. code to the Users model (validations, helper methods to generate and reset session tokens, encrypted password logic)

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -13,4 +13,43 @@
 #
 
 class User < ApplicationRecord
+  attr_reader :password
+  
+  validates :name, :email, :session_token, :balance, presence: true
+  validates :email, :session_token, uniqueness: true
+  validates :password_digest, presence: { message: 'Your password can\'t be blank.' }
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    return user if user && user.is_password?(password)
+
+    nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
 end
