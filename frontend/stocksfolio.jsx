b65d102792bfd9e1cff391b9827cf433142cb5ae
Mannan Kasliwal <mkasliw1@binghamton.edu>
2020-02-17 19:49:36 -0500

    Bootstrap user and put info in slice of states if already logged in, create App and Root components to start basic structuring of component hierarchy, components will have access to Provider and HashRouter methods

--- frontend/stocksfolio.jsx
+++ frontend/stocksfolio.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-// TODO : for testing only
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to Stocksfolio</h1>, root);
 
-  const store = configureStore();
+  let store;
+
+  // bootstrapping the user
+  // check if a user is already logged in and on the window (done in the backend)
+  if (window.currentUser) {
+    // preload the state with the user's data and put it in the users slice
+      // of state and the ID in the session slice of state
+    const preloadedState = {
+      entities: { users: { [window.currentUser.id]: window.currentUser } },
+      session: { currentUserId: window.currentUser.id }
+    };
+
+    store = configureStore(preloadedState);
+
+    // remove the currentUser object from the window
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
   // TODO : for testing only
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+
+  ReactDOM.render(<Root store={store} />, root);
 });
