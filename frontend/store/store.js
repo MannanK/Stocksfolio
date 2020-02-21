37da4ee0e94b23f60aa282df60d15909a98106be
Mannan Kasliwal <mkasliw1@binghamton.edu>
2020-02-20 19:19:22 -0500

    Modify package.json to install 'redux-logger' only in development, modify store.js to add 'logger' as a middleware only in development

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+
+const middlewares = [thunk];
+if (process.env.NODE_ENV !== "production") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
 
 // configureStore will return a new store with the rootReducer and
   // preloadedState (if any) passed in
 // thunk is a middleware we need for future AJAX calls and other async
   //redux actions, and logger is for debugging purposes
 const configureStore = (preloadedState = {}) => (
-  createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
 );
 
 export default configureStore;
